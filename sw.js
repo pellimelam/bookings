955a2c12e47bb8bed00a58aaf1da249a06b99bf2
pellimelam <pellimelam01@gmail.com>
2026-03-27 02:30:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-self.addEventListener("install", e=>{
-self.skipWaiting();
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", ()=>{});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // Always go to network (no caching)
+  event.respondWith(fetch(event.request));
+});
